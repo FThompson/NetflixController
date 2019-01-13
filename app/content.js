202749be4db5bcc078e453559c6ee3008fd8c724
Finn Thompson <nnifinn@gmail.com>
2019-01-12 23:24:43 -0800

    Created Slider class to handle each slider.

--- app/content.js
+++ app/content.js
@@ -1,65 +1,56 @@
 class NetflixController {
     constructor() {
-        // this.row = 1
-        // this.col = 0
-        // this.slider = document.querySelector('#row-1 .slider-item-0')
-        this.selectPosition(1, 0)
-        // this.selectedElement = document.querySelector('#title-card-1-0 img.boxart-image')
-        // selectElement(this.selectedElement)
+        this.slider = new Slider(1)
     }
 
-    selectPosition(row, col) {
-        let element = document.querySelector(`#title-card-${row}-${col} img.boxart-image`)
-        this.selectElement(element)
-        this.row = row
-        this.col = col
-    }
-
-    selectElement(element) {
-        if (this.selectedElement) {
-            let mouseout = new MouseEvent('mouseout', {bubbles: true})
-            this.selectedElement.dispatchEvent(mouseout)
+    handleButtonPress(i) {
+        if (i == 14) {
+            controller.slider.previous()
+        } else if (i == 15) {
+            controller.slider.next()
         }
-        let mouseover = new MouseEvent('mouseover', {bubbles: true})
-        element.dispatchEvent(mouseover)
-        this.selectedElement = element
-    }
-
-    selectLeftElement() {
-        // this.selectElement(this.s)
-        this.selectPosition(this.row, this.col - 1)
-    }
-
-    selectRightElement() {
-        this.selectPosition(this.row, this.col + 1)
     }
 }
 
-// class Slider {
-//     constructor(element) {
-//         this.slider = element
-//     }
+class Slider {
+    constructor(row) {
+        let slider = document.querySelector(`#row-${row} .slider-item-0`)
+        this.selectSlider(slider)
+    }
 
-//     getCardElement() {
-//         return this.slider
-//     }
-// }
+    selectSlider(slider) {
+        if (this.slider) {
+            let mouseout = new MouseEvent('mouseout', {bubbles: true})
+            this.getEventElement(this.slider).dispatchEvent(mouseout)
+            // setTimeout(100, () => this.slider.style.zIndex = '1')
+        }
+        let mouseover = new MouseEvent('mouseover', {bubbles: true})
+        this.getEventElement(slider).dispatchEvent(mouseover)
+        // setTimeout(100, () => slider.style.zIndex = '4')
+        this.slider = slider
+    }
+
+    getEventElement(slider) {
+        return slider.querySelector('img.boxart-image')
+    }
+
+    next() {
+        if (this.slider.nextElementSibling) {
+            this.selectSlider(this.slider.nextElementSibling)
+        }
+    }
+
+    previous() {
+        if (this.slider.previousElementSibling) {
+            this.selectSlider(this.slider.previousElementSibling)
+        } // else vibrate? cannot move slider
+    }
+}
 
 controller = new NetflixController()
 gamepads.addEventListener('connect', (gamepad) => {
     console.log('gamepad connected')
-    console.log(gamepad)
-    gamepad.addEventListener('buttonrelease', (i) => {
-        console.log('released button ' + i)
-    })
-    gamepad.addEventListener('buttonpress', (i) => {
-        console.log('pressed ' + i)
-        if (i == 14) {
-            controller.selectLeftElement()
-        } else if (i == 15) {
-            controller.selectRightElement()
-        }
-    })
+    gamepad.addEventListener('buttonpress', controller.handleButtonPress)
 })
 gamepads.start()
 
